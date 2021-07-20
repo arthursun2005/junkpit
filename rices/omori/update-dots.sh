9bb1a01fcf7f4b71c2b6d2d9d328a18f82540ea5
dorian-chan <dorianchan.cow@gmail.com>
2021-07-20 12:13:20 +1000

    r

--- rices/omori/update-dots.sh
+++ rices/omori/update-dots.sh
@@ -4,3 +4,7 @@ cp ~/.config/i3/config i3-config
 cp ~/.config/conky/conky.conf conky-config
 cp /usr/local/share/fonts/OMORI_GAME2.ttf OMORI_GAME2.ttf
 cp ~/.config/kitty/kitty.conf kitty-config
+cp ~/.p10k.zsh .p10k.zsh
+cp ~/.zshrc .zshrc
+cp ~/Pictures/Wallpapers/omori1.jpg omori1.jpg
+
